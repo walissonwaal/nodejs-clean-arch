aa1591304e050d1b2f5d07acda4cf029cec64564
Walisson Gomes <walissonwaal@gmail.com>
2024-10-06 14:40:10 -0300

    Adding update mathods

--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -1,4 +1,4 @@
-import { Entity } from "@/shared/domain/entities/entity"
+import { Entity } from '@/shared/domain/entities/entity'
 
 export type UserProps = {
   name: string
@@ -8,13 +8,28 @@ export type UserProps = {
 }
 
 export class UserEntity extends Entity<UserProps> {
-  constructor(public readonly props: UserProps, id?: string) {
+  constructor(
+    public readonly props: UserProps,
+    id?: string,
+  ) {
     super(props, id)
     this.props.createdAt = this.props.createdAt ?? new Date()
   }
 
+  update(value: string): void {
+    this.name = value
+  }
+
+  updatePassword(value: string): void {
+    this.password = value
+  }
+
   get name(): string {
     return this.props.name
+  }
+
+  private set name(value: string) {
+    this.props.name = value
   }
 
   get email(): string {
@@ -23,6 +38,10 @@ export class UserEntity extends Entity<UserProps> {
 
   get password(): string {
     return this.props.password
+  }
+
+  private set password(value: string) {
+    this.props.password = value
   }
 
   get createdAt(): Date {
